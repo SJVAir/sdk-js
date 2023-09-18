c64214d0d94bfabd49d43903e5852047ad8dd09a
Alexander McCormick <alexandermccormick@protonmail.com>
2023-09-18 16:23:24 -0700

    adds attribute to detect background color and moves styles into shadow dom

--- web-components/sensor-scatterplot/src/main.ts
+++ web-components/sensor-scatterplot/src/main.ts
@@ -1,14 +1,23 @@
-import { BaseElement, Component, ElementChanges, Input, Query } from "@tstk/web-components";
+import { BaseElement, Component, ElementChanges, Input } from "@tstk/web-components";
 import { fetchMonitorDetails } from "@sjvair/api";
 import { fetchChartData } from "./ChartData";
 import { appendChart } from "./Chart";
-import "uplot/dist/uPlot.min.css";
+import uPlotStyles from "uplot/dist/uPlot.min.css?inline";
+
+/**
+ * uPlot specific stylesheet
+ */
+const styles = new CSSStyleSheet();
+styles.replaceSync(uPlotStyles)
 
 /**
  * A web component for generating scatterPlot graphs with monitor sensor data
  */
 @Component("scatter-plot")
 export class ScatterPlot extends BaseElement {
+  @Input(true)
+  detectBackground!: boolean;
+
   /**
    * The ID of the monitor whose sensors we will graph
    */
@@ -27,11 +36,7 @@ export class ScatterPlot extends BaseElement {
   @Input(true)
   timestampLte?: string;
 
-  /**
-   * The target container for the uPlot graph
-   */
-  @Query("#chartContainer")
-  container!: HTMLDivElement;
+  shadowRoot: ShadowRoot;
 
   /**
    * The component template
@@ -40,18 +45,40 @@ export class ScatterPlot extends BaseElement {
     return `<div id="chartContainer"></div>`;
   }
 
+  /**
+   * Creates a new instance of ScatterPlot
+   *
+   * @returns A new instance of ScatterPlot
+   */
+  constructor() {
+    super();
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+  }
+
+  /**
+   * Set initial styles for ScatterPlot
+   */
   protected onConnected() {
     this.style.display = "block";
+    this.shadowRoot.adoptedStyleSheets = [styles];
   }
 
   /**
    * Initial rendering and re-renders uPlot chart when attribute values vhange
    */
   protected async onChanges(changes: ElementChanges): Promise<void> {
-    if (changes.has("monitorId") || changes.has("timestampGte") || changes.has("timestampLte")) {
+    const changeHappened = changes.has("detectBackground")
+      || changes.has("monitorId")
+      || changes.has("timestampGte")
+      || changes.has("timestampLte");
+
+    if (changeHappened) {
+      const detectBackground = changes.get("detectBackground")?.newValue ?? false;
       const monitorId = changes.get("monitorId")?.newValue ?? this.monitorId;
       const timestampGte = changes.get("timestampGte")?.newValue ?? this.timestampGte;
       const timestampLte = changes.get("timestampLte")?.newValue ?? this.timestampLte;
+
+      const container = document.createElement("div")
       const { width, height } = this.getBoundingClientRect();
 
       const { name, sensors} = await fetchMonitorDetails(monitorId);
@@ -62,13 +89,17 @@ export class ScatterPlot extends BaseElement {
         timestampLte
       });
 
-      this.container.innerHTML = "";
-      appendChart(this.container, data, {
+      this.shadowRoot.innerHTML = "";
+      this.shadowRoot.append(container);
+
+      appendChart(container, data, {
+        detectBackground,
         monitorName: name,
         sensors,
-        height,
+        height: height - 90,
         width
       });
+
     }
   }
 }
