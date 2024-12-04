fef3ac28a2aac1fc7dd19e2d455a083dca4f5742
Alexander McCormick <alexandermccormick@protonmail.com>
2024-12-03 17:05:33 -0800

    adds tests for monitor details

--- api/lib/fetch_monitor_details/mod.ts
+++ api/lib/fetch_monitor_details/mod.ts
@@ -2,6 +2,17 @@ import { getApiUrl } from "../http/mod.ts";
 import type { MonitorData } from "../types.ts";
 
 /**
+ * Constructs the URL for getting the "monitors/${monitorID}/entries" api endpoint.
+ *
+ * @param config An object containing the monitor ID and search parameters
+ *
+ * @returns An instance of URL configured for the "monitors/${monitorID}/entries" api endpoint.
+ */
+export function getMonitorDetailsUrl(monitorId: string): URL {
+  return getApiUrl(`monitors/${monitorId}`);
+}
+
+/**
  * Fetch details about a monitor
  *
  * @param monitorId The ID of the requested monitor
@@ -11,7 +22,7 @@ import type { MonitorData } from "../types.ts";
 export async function fetchMonitorDetails(
   monitorId: string,
 ): Promise<MonitorData> {
-  const url = getApiUrl(`monitors/${monitorId}`);
+  const url = getMonitorDetailsUrl(monitorId);
   return await fetch(url)
     .then((raw) => raw.json())
     .then((res) => res.data)
--- api/lib/fetch_monitor_details/mod_test.ts
+++ api/lib/fetch_monitor_details/mod_test.ts
@@ -1,5 +1,44 @@
-import { assertEquals } from "@std/assert";
+import { assertEquals, fail } from "@std/assert";
+import { fetchMonitorDetails, getMonitorDetailsUrl } from "./mod.ts";
+import { validateMonitorSchema } from "../schemas/monitor.ts";
+import type { MonitorData } from "../types.ts";
 
-Deno.test("Fetch Monitor Details", () => {
-  assertEquals(true, false);
+Deno.test({
+  name: "Module: Fetch Monitor Details",
+  permissions: { net: true },
+  async fn(t) {
+    const monitorId = "xudEmbncQ7iqwy3sZ0jZvQ";
+
+    async function getDetails(): Promise<MonitorData> {
+      return await fetchMonitorDetails(monitorId)
+        .catch((err) => {
+          console.error(err);
+          fail("Monitor entries request failed");
+        });
+    }
+
+    await t.step("Build fetch monitor details url", () => {
+      const url = getMonitorDetailsUrl(monitorId);
+
+      assertEquals(url.origin, "https://www.sjvair.com");
+      assertEquals(
+        url.pathname,
+        "/api/1.0/monitors/xudEmbncQ7iqwy3sZ0jZvQ/",
+      );
+    });
+
+    await t.step("Fetch monitor details", async () => {
+      const monitor = await getDetails();
+      assertEquals(monitor.name, "CCA Root Access Hackerspace #2");
+    });
+
+    await t.step("Validate monitor data", async () => {
+      const details = await getDetails();
+      validateMonitorSchema(details, (errors, entry) => {
+        console.error(errors);
+        console.error(entry);
+        fail("Monitor entry did not pass schema validation");
+      });
+    });
+  },
 });
