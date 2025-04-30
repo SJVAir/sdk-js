78dcd257b6bc059bc8cf1f61a003b40a68709156
Alexander McCormick <alexandermccormick@protonmail.com>
2025-04-30 10:23:20 -0700

    converts to single repo

--- api-v1/lib/monitors/fetch_monitor_entries/mod.ts
+++
@@ -1,5 +0,0 @@
-export * from "./request_builders.ts";
-export * from "./request_handlers.ts";
-export * from "./requests.ts";
-export * from "./types.ts";
-
--- api-v2/mod.ts
+++
@@ -1,5 +0,0 @@
-export * from "./lib/fetch_monitors/mod.ts";
-export * from "./lib/fetch_closest_monitor/mod.ts";
-export * from "./lib/fetch_monitor_details/mod.ts";
-export * from "./lib/fetch_monitor_entries/mod.ts";
-export * from "./lib/types.ts";
