d73de1c0548e7e108850afc084454460cbe48f91
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-08 13:44:47 -0700

    Refactors entries module exports

--- lib/monitors/fetch_monitor_entries/mod.ts
+++ lib/monitors/fetch_monitor_entries/mod.ts
@@ -1,5 +1,19 @@
-export * from "./request_builders.ts";
-export * from "./request_handlers.ts";
-export * from "./requests.ts";
-export * from "./types.ts";
+import { fetchMonitorEntriesPage } from "./requests.ts";
+import { gatherMonitorEntries } from "./response_handlers.ts";
+import type { MonitorEntries } from "../types.ts";
+import type { MonitorEntryRequestConfig } from "./types.ts";
 
+/**
+ * Fetch all pages of entries for a given monitor, using native fetch api
+ *
+ * @param config An object containing the monitor ID and search parameters
+ *
+ * @returns An array of monitor entries
+ */
+export async function fetchMonitorEntries<
+  T extends MonitorEntryRequestConfig,
+>(
+  config: T,
+): Promise<Array<MonitorEntries[T["field"]]>> {
+  return await gatherMonitorEntries(config, fetchMonitorEntriesPage);
+}
