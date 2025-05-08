5ec53734af5084998dd34dafa7638a7721eaf7ba
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-08 13:43:34 -0700

    Updates entries requests with new types

--- lib/monitors/fetch_monitor_entries/requests.ts
+++ lib/monitors/fetch_monitor_entries/requests.ts
@@ -1,6 +1,5 @@
 import { apiRequest } from "$http";
 import { getMonitorEntriesUrl } from "./request_builders.ts";
-//import { gatherMonitorEntries } from "./response_handlers.ts";
 import type {
   FetchMonitorEntriesResponse,
   MonitorEntryRequestConfig,
@@ -14,23 +13,12 @@ import type {
  *
  * @returns An array of monitor entries
  */
-export async function fetchMonitorEntriesPage(
-  config: MonitorEntryRequestConfig,
-): Promise<FetchMonitorEntriesResponse> {
+export async function fetchMonitorEntriesPage<
+  T extends MonitorEntryRequestConfig,
+>(
+  config: T,
+): Promise<FetchMonitorEntriesResponse<T["field"]>> {
   const url = getMonitorEntriesUrl(config);
 
-  return await apiRequest<MonitorEntryRequestResponse>(url);
+  return await apiRequest<MonitorEntryRequestResponse<T["field"]>>(url);
 }
-
-/**
- * Fetch all pages of entries for a given monitor, using native fetch api
- *
- * @param config An object containing the monitor ID and search parameters
- *
- * @returns An array of monitor entries
- */
-//export async function fetchMonitorEntries(
-//  config: MonitorEntryRequestConfig,
-//): Promise<Array<MonitorEntry>> {
-//  return await gatherMonitorEntries(config, fetchMonitorEntriesPage);
-//}
