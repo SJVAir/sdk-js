0c7d21735a43c3975f3a2729559be78a3c00841a
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-08 19:10:38 -0700

    Updates initial tests for v2 of api

--- lib/monitors/fetch_monitor_entries/mod_test.ts
+++ lib/monitors/fetch_monitor_entries/mod_test.ts
@@ -1,12 +1,15 @@
 import { assertEquals, fail } from "@std/assert";
 import { origin, setOrigin } from "$http";
-//import { validateMonitorEntrySchema } from "../schemas/monitor.ts";
-import { validateMonitorEntryRequestResponse } from "../schemas/monitor_entries_request_response.ts";
-//import type { MonitorEntry } from "../types.ts";
+import { validateMonitorEntryRequestResponseSchema } from "../schemas/monitor_entries_request_response.ts";
 import { DEFAULT_DISPLAY_FIELD } from "../constants.ts";
 import { getMonitorEntriesUrl } from "./request_builders.ts";
 import { fetchMonitorEntriesPage } from "./requests.ts";
-import type { MonitorEntryRequestConfig } from "./types.ts";
+import type { MonitorDataField } from "../types.ts";
+import type {
+  MonitorEntryRequestConfig,
+  MonitorEntryRequestResponse,
+} from "./types.ts";
+import { gatherMonitorEntries } from "./response_handlers.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
@@ -28,56 +31,79 @@ const requestConfig: MonitorEntryRequestConfig = {
 //  assertGreater(entries.length, 0);
 //}
 
+function validateMonitorEntryRequestResponse(
+  response: MonitorEntryRequestResponse<MonitorDataField>,
+) {
+  validateMonitorEntryRequestResponseSchema(response, (errors, data) => {
+    console.error(errors);
+    console.error(data);
+    fail("Monitor entries page did not pass schema validation");
+  });
+}
+
 Deno.test({
   name: "Module: Fetch Monitor Entries",
   permissions: { net: true },
   async fn(t) {
-    await t.step("Build fetch monitor entries url", () => {
-      const url = getMonitorEntriesUrl(requestConfig);
+    await t.step(
+      "Build fetch monitor entries request",
+      async (t2) => {
+        const canBuildUrl = await t2.step(
+          "Get URL",
+          () => {
+            const url = getMonitorEntriesUrl(requestConfig);
 
-      assertEquals(url.origin, origin);
-      assertEquals(
-        url.pathname,
-        `/api/2.0/monitors/${requestConfig.monitorId}/entries/${DEFAULT_DISPLAY_FIELD}/`,
-      );
-      assertEquals(url.searchParams.has("page"), true);
-      assertEquals(url.searchParams.has("sensor"), true);
-      assertEquals(url.searchParams.has("timestamp__gte"), true);
-      assertEquals(url.searchParams.has("timestamp__lte"), true);
-    });
+            assertEquals(url.origin, origin);
+            assertEquals(
+              url.pathname,
+              `/api/2.0/monitors/${requestConfig.monitorId}/entries/${DEFAULT_DISPLAY_FIELD}/`,
+            );
+            assertEquals(url.searchParams.has("page"), true);
+            assertEquals(url.searchParams.has("sensor"), true);
+            assertEquals(url.searchParams.has("timestamp__gte"), true);
+            assertEquals(url.searchParams.has("timestamp__lte"), true);
+          },
+        );
 
-    const fetchEntriesPageSuccess = await t.step(
-      "Fetch single monitor entries page",
-      async (st) => {
-        const response = await fetchMonitorEntriesPage(requestConfig)
-          .catch((err) => {
-            console.error(err);
-            fail("failed to fetch single monitor entries page");
-          });
+        await t2.step({
+          name: "Fetch single monitor entries page",
+          ignore: !canBuildUrl,
+          async fn(t3) {
+            const response = await fetchMonitorEntriesPage(requestConfig)
+              .catch((err) => {
+                console.error(err);
+                fail("failed to fetch single monitor entries page");
+              });
 
-        assertEquals(response.status, 200);
+            assertEquals(response.status, 200);
 
-        await st.step("Validate single entries page", () => {
-          validateMonitorEntryRequestResponse(response.body, (errors, data) => {
-            console.error(errors);
-            console.error(data);
-            fail("Monitor entries page did not pass schema validation");
-          });
+            const validFirstPage = await t3.step(
+              "Validate single entries page",
+              () => validateMonitorEntryRequestResponse(response.body),
+            );
+
+            await t3.step({
+              name: "Aggregate monitor entries from custom request",
+              ignore: !validFirstPage,
+              async fn(t4) {
+                const entries = await gatherMonitorEntries(
+                  requestConfig,
+                  fetchMonitorEntriesPage,
+                );
+
+                assertEquals(Array.isArray(entries), true);
+
+                // Left off here
+                //await t4.step(
+                //  "Validate entries",
+                //  () => validateEntries(),
+                //);
+              },
+            });
+          },
         });
       },
     );
-
-    //await t.step({
-    //  name: "Aggregate monitor entries from custom request",
-    //  ignore: !fetchEntriesPageSuccess,
-    //  async fn() {
-    //    const entries = await gatherMonitorEntries(
-    //      requestConfig,
-    //      fetchMonitorEntriesPage,
-    //    );
-    //    validateEntries(entries);
-    //  },
-    //});
 
     //const fetchentriesSuccess = await t.step({
     //  name: "Fetch all monitor entries",
