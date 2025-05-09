c9cbc5fe7e54e481e4777fe10beb8da822fc9d31
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-08 17:26:30 -0700

    Refactors fetch monitors module test

--- lib/monitors/fetch_monitors/mod_test.ts
+++ lib/monitors/fetch_monitors/mod_test.ts
@@ -5,73 +5,78 @@ import { getMonitorsUrl } from "./request_builders.ts";
 import { fetchMonitorsHandler } from "./response_handlers.ts";
 import { fetchMonitors } from "./requests.ts";
 import { getMonitors } from "./mod.ts";
-import type { FetchMonitorsResponse } from "./types.ts";
+import type { MonitorData } from "../types.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
+}
+
+function validateMonitorData(monitors: Array<MonitorData>) {
+  validateMonitorDataSchema(monitors, (errors, monitor) => {
+    console.error(errors);
+    console.error(monitor);
+    fail("Monitor data did not pass schema validation");
+  });
 }
 
 Deno.test({
   name: "Module: Fetch Monitors",
   permissions: { net: true },
   async fn(t) {
-    await t.step("Build fetch monitors request", async (st) => {
-      let rawResponse: FetchMonitorsResponse;
-
-      await st.step("Get URL", () => {
+    const success = await t.step("Build fetch monitors request", async (t2) => {
+      await t2.step("Get URL", () => {
         const url = getMonitorsUrl();
 
         assertEquals(url.href, `${origin}/api/2.0/monitors/`);
       });
 
-      const rawResponseSuccess = await st.step(
+      await t2.step(
         "Fetch raw response",
-        async () => {
-          rawResponse = await fetchMonitors();
+        async (t3) => {
+          const rawResponse = await fetchMonitors();
 
           assertEquals(rawResponse.status, 200);
+
+          await t3.step(
+            "Handle raw response",
+            async (t4) => {
+              const monitors = fetchMonitorsHandler(rawResponse);
+
+              assertEquals(Array.isArray(monitors), true);
+
+              await t4.step(
+                "Validate monitor data",
+                () => validateMonitorData(monitors),
+              );
+            },
+          );
         },
       );
-
-      await st.step({
-        name: "Handle raw response",
-        ignore: !rawResponseSuccess,
-        fn() {
-          const monitors = fetchMonitorsHandler(rawResponse);
-
-          assertEquals(Array.isArray(monitors), true);
-        },
-      });
     });
 
-    await t.step("Prebuilt fetch monitors request", async (st) => {
-      let monitors: Awaited<ReturnType<typeof getMonitors>>;
+    await t.step({
+      name: "Prebuilt fetch monitors request",
+      ignore: !success,
+      async fn(t2) {
+        await t2.step(
+          "Fetch all monitors",
+          async (t3) => {
+            const monitors = await getMonitors()
+              .catch((err) => {
+                console.error("Error(fetch all monitors):", err);
+                fail(
+                  "Prebuilt fetchMonitors request failed! Error data should be displayed",
+                );
+              });
+            assertEquals(Array.isArray(monitors), true);
 
-      const fetchMonitorsSuccess = await st.step(
-        "Fetch all monitors",
-        async () => {
-          monitors = await getMonitors()
-            .catch((err) => {
-              console.error("Error(fetch all monitors):", err);
-              fail(
-                "Prebuilt fetchMonitors request failed! Error data should be displayed",
-              );
-            });
-          assertGreater(monitors.length, 0);
-        },
-      );
-
-      await st.step({
-        name: "Validate monitor data",
-        ignore: !fetchMonitorsSuccess,
-        fn() {
-          validateMonitorDataSchema(monitors, (errors, monitor) => {
-            console.error(errors);
-            console.error(monitor);
-            fail("Monitor data did not pass schema validation");
-          });
-        },
-      });
+            await t3.step(
+              "Validate monitor data",
+              () => validateMonitorData(monitors),
+            );
+          },
+        );
+      },
     });
   },
 });
