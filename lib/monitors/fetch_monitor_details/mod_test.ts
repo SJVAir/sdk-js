caf1cf1adce1e9d34d6c881e2a96db5c9cf925f5
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-08 17:58:40 -0700

    Refactors fetch monitor details tests

--- lib/monitors/fetch_monitor_details/mod_test.ts
+++ lib/monitors/fetch_monitor_details/mod_test.ts
@@ -5,55 +5,63 @@ import { fetchMonitorDetailsHandler } from "./response_handlers.ts";
 import { fetchMonitorDetails } from "./requests.ts";
 import { getMonitorDetails } from "./mod.ts";
 import { validateMonitorDetailsSchema } from "../schemas/monitor.ts";
+import type { MonitorDetails } from "../types.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
+}
+
+const monitorId = "HmeeUr66RpKHFx64nBV6hQ";
+
+function validateMonitorDetails(
+  details: MonitorDetails | Array<MonitorDetails>,
+) {
+  validateMonitorDetailsSchema(details, (errors, monitor) => {
+    console.error(errors);
+    console.error(monitor);
+    fail("Monitor data did not pass schema validation");
+  });
 }
 
 Deno.test({
   name: "Module: Fetch Monitor Details",
   permissions: { net: true },
   async fn(t) {
-    const monitorId = "HmeeUr66RpKHFx64nBV6hQ";
+    const success = await t.step(
+      "Build fetch monitor details request",
+      async (t2) => {
+        const buildUrlSuccess = await t2.step(
+          "Build fetch monitor details url",
+          () => {
+            const url = getMonitorDetailsUrl(monitorId);
 
-    const buildUrlSuccess = await t.step(
-      "Build fetch monitor details url",
-      () => {
-        const url = getMonitorDetailsUrl(monitorId);
+            assertEquals(url.href, `${origin}/api/2.0/monitors/${monitorId}/`);
+          },
+        );
 
-        assertEquals(url.href, `${origin}/api/2.0/monitors/${monitorId}/`);
+        await t2.step({
+          name: "Fetch raw response",
+          ignore: !buildUrlSuccess,
+          async fn(t3) {
+            const response = await fetchMonitorDetails(monitorId);
+            assertEquals(response.status, 200);
+
+            await t3.step("Handle raw response", () => {
+              const details = fetchMonitorDetailsHandler(response);
+              validateMonitorDetails(details);
+              assertEquals(details.id, monitorId);
+            });
+          },
+        });
       },
     );
 
-    const fetchMonitorDetailsSuccess = await t.step({
-      name: "Fetch raw response",
-      ignore: !buildUrlSuccess,
-      async fn(st) {
-        const response = await fetchMonitorDetails(monitorId);
-        assertEquals(response.status, 200);
-
-        await st.step("Handle raw response", () => {
-          const details = fetchMonitorDetailsHandler(response);
-          validateMonitorDetailsSchema(details, (errors, monitor) => {
-            console.error(errors);
-            console.error(monitor);
-            fail("Monitor data did not pass schema validation");
-          });
-          assertEquals(details.id, monitorId);
-        });
-      },
-    });
-
     await t.step({
       name: "Prebuild request and handler",
-      ignore: !fetchMonitorDetailsSuccess,
+      ignore: !success,
       async fn() {
         const details = await getMonitorDetails(monitorId);
-        validateMonitorDetailsSchema(details, (errors, monitor) => {
-          console.error(errors);
-          console.error(monitor);
-          fail("Monitor data did not pass schema validation");
-        });
+        validateMonitorDetails(details);
         assertEquals(details.id, monitorId);
       },
     });
