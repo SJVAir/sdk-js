8dcdfc98823cf24ffa6c9769a29e4e15fbbacd61
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-14 14:26:03 -0700

    Updates DEFAULT_DISPLAY_FIELD to allow use as literal type

--- lib/monitors/constants.ts
+++ lib/monitors/constants.ts
@@ -1,3 +1,3 @@
 import type { MonitorDataField } from "./types.ts";
 
-export const DEFAULT_DISPLAY_FIELD: MonitorDataField = "pm25";
+export const DEFAULT_DISPLAY_FIELD = "pm25" satisfies MonitorDataField;
