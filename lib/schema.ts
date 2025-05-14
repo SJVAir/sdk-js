da5bda5ac79722531844c0f3c2c422c4145d07e4
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-14 12:04:09 -0700

    Adds options object for schema validation builder

--- lib/schema.ts
+++ lib/schema.ts
@@ -5,22 +5,29 @@ export type SchemaValidationFailureHandler<T> = (
   response: T,
 ) => void;
 
-export function schemaValidator<T>(schema: JSONSchemaType<T>) {
+export interface SchemaValidatorOptions {
+  checkArray: boolean;
+}
+
+export function schemaValidator<T>(
+  schema: JSONSchemaType<T>,
+  options?: SchemaValidatorOptions,
+) {
   return function (
     data: T | Array<T>,
-    failureHandle: SchemaValidationFailureHandler<T>,
+    failureHandle: SchemaValidationFailureHandler<T | Array<T>>,
   ) {
     const ajv = new Ajv();
     const validate = ajv.compile(schema);
 
-    const process = (item: T) => {
+    const process = (item: T | Array<T>) => {
       const valid = validate(item);
       if (!valid) {
         failureHandle(validate.errors, item);
       }
     };
 
-    if (Array.isArray(data)) {
+    if (Array.isArray(data) && !options?.checkArray) {
       for (const item of data) {
         process(item);
       }
