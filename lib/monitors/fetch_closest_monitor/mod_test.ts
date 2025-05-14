b8455d95c1863cdfef223c808934bd9ac393a53f
Alexander McCormick <alexandermccormick@protonmail.com>
2025-05-14 14:32:15 -0700

    Adds closest monitor module tests

--- lib/monitors/fetch_closest_monitor/mod_test.ts
+++ lib/monitors/fetch_closest_monitor/mod_test.ts
@@ -1,113 +1,116 @@
-import { assertEquals } from "@std/assert";
-import { setOrigin } from "$http";
-import {
-  fetchClosestMonitor,
-  getClosestMonitorUrl,
-  validateClosestMonitorResponse,
-} from "./mod.ts";
-import { isValidClosestMonitor } from "./validation.ts";
-import type { MonitorData } from "../types.ts";
+import { assertEquals, fail } from "@std/assert";
+import { origin, setOrigin } from "$http";
+import { DEFAULT_DISPLAY_FIELD } from "../constants.ts";
+import { validateMonitorLatestSchema } from "../schemas/monitor.ts";
+import { coordinates } from "../test_constants.ts";
+import { validateClosestMonitors } from "./validation.ts";
+import { getClosestMonitorUrl } from "./request_builders.ts";
+import { fetchClosestMonitor } from "./requests.ts";
+import type { MonitorEntries, MonitorLatest } from "../types.ts";
+import { fetchClosestMonitorsHandler } from "./response_handlers.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
 }
 
-const raMonitor: MonitorData = {
-  id: "xudEmbncQ7iqwy3sZ0jZvQ",
-  name: "CCA Root Access Hackerspace #2",
-  device: "PA-II",
-  is_active: true,
-  is_sjvair: true,
-  position: { type: "Point", coordinates: [-119.798676, 36.76274] },
-  sensors: ["a", "b"],
-  last_active_limit: 3600,
-  location: "outside",
-  latest: {
-    timestamp: "2024-11-20T23:02:11Z",
-    sensor: "a",
-    celsius: "26.1",
-    fahrenheit: "79.0",
-    humidity: "13.0",
-    pressure: "1005.11",
-    pm10: "3.95",
-    pm25: "0",
-    pm100: "5.77",
-    pm25_reported: "5.29",
-    pm25_avg_15: "-6.98",
-    pm25_avg_60: "0",
-    particles_03um: "1679.00",
-    particles_05um: "256.00",
-    particles_100um: "0.00",
-    particles_10um: "24.00",
-    particles_25um: "3.00",
-    particles_50um: "0.00",
-    pm100_standard: null,
-    pm10_standard: null,
-    pm25_standard: null,
-  },
-  county: "Fresno",
-  data_source: { name: "PurpleAir", url: "https://www2.purpleair.com/" },
-  data_providers: [{
-    name: "PurpleAir",
-    url: "https://www2.purpleair.com/",
-  }],
-  distance: 0,
-  purple_id: 8892,
-};
+const { longitude, latitude } = coordinates;
+
+type MonitorLatestObject = MonitorLatest<keyof MonitorEntries>;
+
+function validateMonitorLatest(
+  monitors: MonitorLatestObject | Array<MonitorLatestObject>,
+) {
+  validateMonitorLatestSchema(
+    monitors,
+    (errors, monitor) => {
+      console.error(errors);
+      console.error(monitor);
+      fail("Monitor data did not pass schema validation");
+    },
+  );
+}
 
 Deno.test({
   name: "Module: Fetch Closest Monitor",
   permissions: { net: true },
   async fn(t) {
-    const [longitude, latitude] = raMonitor.position.coordinates;
+    await t.step(
+      "Build fetch closest monitor request",
+      async (t2) => {
+        const canBuildUrl = await t2.step(
+          "Get URL",
+          () => {
+            const url = getClosestMonitorUrl(
+              DEFAULT_DISPLAY_FIELD,
+              latitude,
+              longitude,
+            );
 
-    await t.step("Build closest monitor url", () => {
-      const [longitude, latitude] = raMonitor.position.coordinates;
-      const url = getClosestMonitorUrl(latitude, longitude);
+            assertEquals(url.origin, origin);
+            assertEquals(
+              url.pathname,
+              "/api/2.0/monitors/pm25/closest/",
+            );
+            assertEquals(url.searchParams.has("latitude"), true);
+            assertEquals(url.searchParams.has("longitude"), true);
+          },
+        );
 
-      assertEquals(
-        url.href,
-        "https://www.sjvair.com/api/1.0/monitors/closest/?latitude=36.76274&longitude=-119.798676",
-      );
-    });
+        await t2.step({
+          name: "Fetch raw response",
+          ignore: !canBuildUrl,
+          async fn(t3) {
+            const response = await fetchClosestMonitor(
+              DEFAULT_DISPLAY_FIELD,
+              latitude,
+              longitude,
+            )
+              .catch((err) => {
+                console.error(err);
+                fail("failed to fetch single monitor entries page");
+              });
 
-    await t.step("Validate Closest Monitor", async (t) => {
-      await t.step("Valid Monitor", () => {
-        const isValid = isValidClosestMonitor(raMonitor);
-        assertEquals(isValid, true);
-      });
+            assertEquals(response.status, 200);
 
-      await t.step("Invalid Monitor", () => {
-        const invalidMonitor = structuredClone(raMonitor);
-        invalidMonitor.latest!.pm25 = "-16";
+            await t3.step(
+              "Handle raw response",
+              async (t4) => {
+                const monitors = fetchClosestMonitorsHandler(response);
 
-        const isValid = isValidClosestMonitor(invalidMonitor);
+                // This simulates the correct response form the endpoint
+                //monitors.map((m) => {
+                //  m.latest = {
+                //    processor: "fake",
+                //    sensor: "none",
+                //    stage: "maybe?",
+                //    timestamp: "nowish",
+                //    value: "2.0",
+                //  };
+                //});
 
-        assertEquals(isValid, false);
-      });
-    });
+                assertEquals(Array.isArray(monitors), true);
 
-    await t.step("Validate Closest Monitor Response", () => {
-      const m1 = structuredClone(raMonitor);
-      m1.name = "Fake Monitor 1";
-      m1.latest!.pm25 = "-16";
+                console.log(monitors);
+                await t4.step("Validate closest monitor", () => {
+                  const closestMonitor = validateClosestMonitors(monitors);
 
-      const m2 = structuredClone(raMonitor);
-      m2.name = "Fake Monitor 2";
-      m2.latest!.pm25 = "9999";
-
-      const m3 = structuredClone(raMonitor);
-      m3.name = "Fake Monitor 3";
-
-      const monitor = validateClosestMonitorResponse([m1, m2, m3]);
-
-      assertEquals(monitor.name, "Fake Monitor 3");
-    });
-
-    await t.step("Fetch Closest Monitor", async () => {
-      const monitor = await fetchClosestMonitor(latitude, longitude);
-
-      assertEquals(monitor.name, "CCA Root Access Hackerspace #2");
-    });
+                  assertEquals(Array.isArray(closestMonitor), false);
+                  // This simulates the correct response form the endpoint
+                  //assertEquals(
+                  //  closestMonitor.name,
+                  //  "CCA Root Access Hackerspace #1",
+                  //);
+                  assertEquals(
+                    closestMonitor.name,
+                    "CCA Root Access Hackerspace #2",
+                  );
+                });
+              },
+            );
+            // STILL NEED TO TEST PREBUILT FUNCTION
+          },
+        });
+      },
+    );
   },
 });
