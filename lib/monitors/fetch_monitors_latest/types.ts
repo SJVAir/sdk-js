848ad6909e156b9a760110026df058c8b0df7303
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-02 15:54:56 -0700

    Adds new DefaultLatestMonitor type

--- lib/monitors/fetch_monitors_latest/types.ts
+++ lib/monitors/fetch_monitors_latest/types.ts
@@ -1,5 +1,11 @@
 import type { APIRequestResponse } from "$http";
+import type { DEFAULT_DISPLAY_FIELD } from "../constants.ts";
 import type { MonitorDataField, MonitorLatest } from "../types.ts";
+
+/**
+ * The default data structure for monitors including a "latest" field
+ */
+export type DefaultLatestMonitor = MonitorLatest<typeof DEFAULT_DISPLAY_FIELD>;
 
 /**
  * The data structure returned from the "/monitors/" endpoint
