43c773f712bb0257e0fdb6b75dcd4f506e9655fd
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-02 15:55:18 -0700

    Adds note for refactoring fetchMonitorsHandler

--- lib/monitors/fetch_monitors/response_handlers.ts
+++ lib/monitors/fetch_monitors/response_handlers.ts
@@ -1,6 +1,9 @@
 import type { MonitorData } from "../types.ts";
 import type { FetchMonitorsResponse } from "./types.ts";
 
+// TODO: This should accept the response body as an argument
+// not the whole response object. This way it is more useful
+// with other http request libs
 export function fetchMonitorsHandler(
   response: FetchMonitorsResponse,
 ): Array<MonitorData> {
