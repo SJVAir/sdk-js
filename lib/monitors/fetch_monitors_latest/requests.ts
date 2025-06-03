9d6c6580f2b1a9c39e1db75efef22ce01ef1a9b6
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-03 12:56:59 -0700

    Converts getMonitorsLatest to generic consolidateMonitorsLatest

--- lib/monitors/fetch_monitors_latest/requests.ts
+++ lib/monitors/fetch_monitors_latest/requests.ts
@@ -1,12 +1,25 @@
+import { consolidateApiRequest } from "$http";
 import { DEFAULT_DISPLAY_FIELD } from "../constants.ts";
 import type { MonitorLatest } from "../types.ts";
-import { fetchMonitorsLatest } from "./fetchers.ts";
-import { fetchMonitorsLatestHandler } from "./response_handlers.ts";
+import { fetchMonitorsLatestPage } from "./fetchers.ts";
+import type {
+  MonitorsLatestRequestConfig,
+} from "./types.ts";
 
 /**
- * Fetches all monitors.
+ * Fetches all monitors with a "latest" entry.
  *
- * @returns An array containing all monitors.
+ * @returns An array containing all monitors with a "latest" entry.
+ */
+export async function consolidateMonitorsLatest<
+  T extends MonitorsLatestRequestConfig,
+>(
+  config: T,
+): Promise<
+  Array<MonitorLatest<T["field"]>>
+> {
+  return await consolidateApiRequest(config, fetchMonitorsLatestPage);
+}
  */
 export async function getMonitorsLatest(): Promise<
   Array<MonitorLatest<typeof DEFAULT_DISPLAY_FIELD>>
