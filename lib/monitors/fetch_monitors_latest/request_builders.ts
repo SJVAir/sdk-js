9017e0f7d896b42942f785e1f69825fc4f7541dc
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-03 12:53:02 -0700

    Updates getMonitorsLatestUrl to use pages

--- lib/monitors/fetch_monitors_latest/request_builders.ts
+++ lib/monitors/fetch_monitors_latest/request_builders.ts
@@ -1,11 +1,33 @@
 import { getApiUrl } from "$http";
-import type { MonitorDataField } from "../types.ts";
+import type { MonitorsLatestRequestConfig } from "./types.ts";
+
+/**
+ * A structure used for parsing MonitorsLatestRequestConfig
+ */
+class MonitorsLatestRequestOptions {
+  [key: string]: string;
+
+  /** The results page number to fetch */
+  page: string;
+
+  /**
+   * Constructs a new MonitorsLatestRequestOptions object
+   *
+   * @param options A user defined MonitorsLatestRequestConfig object
+   */
+  constructor(options: MonitorsLatestRequestConfig) {
+    this.page = options.page?.toString() ?? "1";
+  }
+}
 
 /**
  * Constructs the URL for getting the "monitors" api endpoint.
  *
  * @returns An instance of URL configured for the "monitors" api endpoint.
  */
-export function getMonitorsLatestUrl(entryType: MonitorDataField): URL {
-  return getApiUrl(`monitors/${entryType}/current`);
+export function getMonitorsLatestUrl(
+  config: MonitorsLatestRequestConfig,
+): URL {
+  const params = new MonitorsLatestRequestOptions(config);
+  return getApiUrl(`monitors/${config.field}/current`, params);
 }
