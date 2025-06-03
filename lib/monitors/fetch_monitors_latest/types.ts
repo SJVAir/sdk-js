789d20c8779e68500ed787a3bc9ff04f6e990541
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-03 12:52:01 -0700

    Updates fetch_monitors_latest types to use PaginatedResponse

--- lib/monitors/fetch_monitors_latest/types.ts
+++ lib/monitors/fetch_monitors_latest/types.ts
@@ -1,6 +1,17 @@
-import type { APIRequestResponse } from "$http";
+import type { APIRequestResponse, PaginatedResponse } from "$http";
 import type { DEFAULT_DISPLAY_FIELD } from "../constants.ts";
 import type { MonitorDataField, MonitorLatest } from "../types.ts";
+
+/**
+ * The configuration options for the monitors latest endpoint
+ */
+export interface MonitorsLatestRequestConfig {
+  /** Fields to include on the monitor entry */
+  field: MonitorDataField;
+
+  /** The results page number to fetch */
+  page?: number;
+}
 
 /**
  * The default data structure for monitors including a "latest" field
@@ -11,6 +22,4 @@ export type DefaultLatestMonitor = MonitorLatest<typeof DEFAULT_DISPLAY_FIELD>;
  * The data structure returned from the "/monitors/" endpoint
  */
 export type FetchMonitorsLatestResponse<T extends MonitorDataField> =
-  APIRequestResponse<
-    { data: Array<MonitorLatest<T>> }
-  >;
+  APIRequestResponse<PaginatedResponse<MonitorLatest<T>>>;
