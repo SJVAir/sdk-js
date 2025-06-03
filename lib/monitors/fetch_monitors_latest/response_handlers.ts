628c1c6f6eddfb22dfbf5420818cfcfdc559c9e3
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-03 12:54:09 -0700

    Removes deprecated Monitors Latest response handler

--- lib/monitors/fetch_monitors_latest/response_handlers.ts
+++
@@ -1,9 +0,0 @@
-import type { MonitorDataField, MonitorLatest } from "../types.ts";
-import type { FetchMonitorsLatestResponse } from "./types.ts";
-
-export function fetchMonitorsLatestHandler<T extends MonitorDataField>(
-  response: FetchMonitorsLatestResponse<T>,
-): Array<MonitorLatest<T>> {
-  const { data } = response.body;
-  return (data && Array.isArray(data)) ? data : [];
-}
