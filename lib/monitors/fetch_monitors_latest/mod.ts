ac5fee565c4b6fe4efbdf6551e33891ffe94e568
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-03 12:58:28 -0700

    Updates fetch_monitors_latest module exports

--- lib/monitors/fetch_monitors_latest/mod.ts
+++ lib/monitors/fetch_monitors_latest/mod.ts
@@ -1,5 +1,4 @@
-export { fetchMonitorsLatest } from "./fetchers.ts";
+export { fetchMonitorsLatestPage } from "./fetchers.ts";
 export { getMonitorsLatestUrl } from "./request_builders.ts";
-export { getMonitorsLatest } from "./requests.ts";
-export { fetchMonitorsLatestHandler } from "./response_handlers.ts";
+export { consolidateMonitorsLatest, getMonitorsLatest } from "./requests.ts";
 export * from "./types.ts";
