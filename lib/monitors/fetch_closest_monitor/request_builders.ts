e96153bc8222973d0fe6a8b12f019f2b226a42c5
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-13 14:41:58 -0700

    Updated to use new Monitor types

--- lib/monitors/fetch_closest_monitor/request_builders.ts
+++ lib/monitors/fetch_closest_monitor/request_builders.ts
@@ -1,16 +1,17 @@
 import { getApiUrl } from "$http";
-import type { MonitorDataField } from "../types.ts";
+import type { MonitorEntryType } from "../types.ts";
 
 /**
  * Constructs the URL for getting the "monitors/closest" api endpoint.
  *
+ * @param entryType The type of entry to include in the "latest" field
  * @param latitude The latitudinal coordinate of the desired location
  * @param longitude The longitudinal coordinate of the desired location
  *
  * @returns An instance of URL configured for the "monitors/closest" api endpoint.
  */
 export function getClosestMonitorUrl(
-  entryType: MonitorDataField,
+  entryType: MonitorEntryType,
   latitude: number | string,
   longitude: number | string,
 ): URL {
