a169722b7e88bc9163389baab92fa7b81c73baa0
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-13 15:23:27 -0700

    Updates types and docs

--- lib/monitors/fetch_monitor_details/types.ts
+++ lib/monitors/fetch_monitor_details/types.ts
@@ -4,6 +4,11 @@ import type { MonitorDetails } from "../types.ts";
 /**
  * The data structure returned from the "/monitors/{ MONITOR_ID }/" endpoint
  */
+export type MonitorDetailsResponse = { data: MonitorDetails };
+
+/**
+ * The data structure of the api response returned from the "/monitors/{ MONITOR_ID }/" endpoint
+ */
 export type FetchMonitorDetailssResponse = APIRequestResponse<
-  { data: MonitorDetails }
+  MonitorDetailsResponse
 >;
