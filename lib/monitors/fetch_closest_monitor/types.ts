76494b75210d724f9a39e24dd31b768680b2cbe8
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-13 15:22:59 -0700

    Updates docs

--- lib/monitors/fetch_closest_monitor/types.ts
+++ lib/monitors/fetch_closest_monitor/types.ts
@@ -1,13 +1,16 @@
 import type { APIRequestResponse, PaginatedResponse } from "$http";
 import type { MonitorClosestType, MonitorEntryType } from "../types.ts";
 
+/**
+ * The data structure returned from the "/monitors/<entry_type>/closest/" endpoint
+ */
 export type ClosestMonitorsResponse<T extends MonitorEntryType> =
   PaginatedResponse<
     MonitorClosestType<T>
   >;
 
 /**
- * The data structure returned from the "/monitors/<entry_type>/closest/" endpoint
+ * The data structure of the api response returned from the "/monitors/<entry_type>/closest/" endpoint
  */
 export type FetchClosestMonitorsResponse<T extends MonitorEntryType> =
   APIRequestResponse<ClosestMonitorsResponse<T>>;
