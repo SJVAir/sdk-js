edff9660225d6aa262d040fe6133fbdaed0160d5
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-17 13:20:47 -0700

    adds explicit types for hms smoke schemas

--- lib/hms_smoke/schema.ts
+++ lib/hms_smoke/schema.ts
@@ -1,6 +1,14 @@
 import * as z from "zod";
 
-export const multiPolygonSchema = z.object({
+export interface MultiPolygonSchema extends
+  z.ZodObject<{
+    type: z.ZodLiteral<"MultiPolygon">;
+    coordinates: z.ZodArray<
+      z.ZodArray<z.ZodArray<z.ZodTuple<[z.ZodNumber, z.ZodNumber]>>>
+    >;
+  }> {}
+
+export const multiPolygonSchema: MultiPolygonSchema = z.object({
   type: z.literal("MultiPolygon"),
   coordinates: z.array(
     z.array(
@@ -11,7 +19,19 @@ export const multiPolygonSchema = z.object({
   ),
 });
 
-export const hmsSmokeSchema = z.object({
+export interface HMSSmokeSchema extends
+  z.ZodObject<{
+    id: z.ZodString;
+    satellite: z.ZodString;
+    density: z.ZodEnum<{ light: "light"; medium: "medium"; heavy: "heavy" }>;
+    end: z.ZodString;
+    start: z.ZodString;
+    date: z.ZodString;
+    geometry: MultiPolygonSchema;
+    is_final: z.ZodBoolean;
+  }> {}
+
+export const hmsSmokeSchema: HMSSmokeSchema = z.object({
   id: z.string(),
   satellite: z.string(),
   density: z.enum(["light", "medium", "heavy"]),
