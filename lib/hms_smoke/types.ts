10f70f19979bdde05cbf6e7f7fa82010fb1fa1f1
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-21 12:39:48 -0700

    MovesShared HMS smoke type to top level module types

--- lib/hms_smoke/types.ts
+++ lib/hms_smoke/types.ts
@@ -1,4 +1,20 @@
 import type { infer as zinfer } from "zod";
+import type { APIRequestResponse, PaginatedResponse } from "$http";
 import type { hmsSmokeSchema } from "./schema.ts";
 
 export type HMSSmokeGeoJSON = zinfer<typeof hmsSmokeSchema>;
+
+/**
+ * The configuration options for the HMS smoke endpoint
+ */
+export interface HMSSmokeRequestConfig {
+  /** The results page number to fetch */
+  page?: number;
+}
+
+/**
+ * The data structure returned from the "/hms-smoke/" endpoint
+ */
+export type FetchHMSSmokeResponse = APIRequestResponse<
+  PaginatedResponse<HMSSmokeGeoJSON>
+>;
