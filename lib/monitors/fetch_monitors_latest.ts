2ebd6bb5ca3d6dcd3dab774ec8f00a75c56fff2c
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-09 17:22:16 -0700

    Reduces fetch_monitors_latest module

---
+++ lib/monitors/fetch_monitors_latest.ts
@@ -0,0 +1,33 @@
+import { apiCall } from "$http";
+import type { MonitorEntryType, MonitorLatestType } from "./types.ts";
+
+/**
+ * The data structure returned from the "/monitors/" endpoint
+ */
+export type MonitorsLatestResponse<T extends MonitorEntryType> = {
+  data: Array<MonitorLatestType<T>>;
+};
+
+/**
+ * Fetches all monitors with a "latest" entry.
+ *
+ * @returns An array containing all monitors with a "latest" entry.
+ */
+export async function getMonitorsLatest<
+  T extends MonitorEntryType,
+>(
+  field: T,
+): Promise<Array<MonitorLatestType<T>>> {
+  return await apiCall<
+    MonitorsLatestResponse<T>,
+    Array<MonitorLatestType<T>>
+  >(
+    `monitors/${field}/current`,
+    (status, body) => {
+      if (status !== 200) {
+        console.error("Unexpected status code:", status);
+      }
+      return body.data;
+    },
+  );
+}
