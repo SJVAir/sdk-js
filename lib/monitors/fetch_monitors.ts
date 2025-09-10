e51b803e9009fc4b31196dfc396c5da910fa5932
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-09 17:22:00 -0700

    Reduces fetch_monitors module

--- lib/monitors/fetch_monitors.ts
+++ lib/monitors/fetch_monitors.ts
@@ -1,30 +1,31 @@
-import { apiRequest, type APIRequestResponse, getApiUrl } from "$http";
+import { apiCall } from "$http";
 import type { MonitorData } from "./types.ts";
 
 /**
  * The data structure returned from the "/monitors/" endpoint
  */
-export type FetchMonitorsResponse = APIRequestResponse<
-  { data: Array<MonitorData> }
->;
-
-// TODO: This should accept the response body as an argument
-// not the whole response object. This way it is more useful
-// with other http request libs
-export function fetchMonitorsHandler(
-  response: FetchMonitorsResponse,
-): Array<MonitorData> {
-  const { data } = response.body;
-  return (data && data.length) ? data : [];
-}
+export type FetchMonitorsResponse = { data: Array<MonitorData> };
 
 /**
  * Fetches all monitors.
  *
  * @returns An array containing all monitors.
  */
-export async function getMonitors(): Promise<FetchMonitorsResponse> {
-  return await apiRequest<{ data: Array<MonitorData> }>(
-    getApiUrl("monitors"),
+export async function getMonitors(): Promise<Array<MonitorData>> {
+  return await apiCall<FetchMonitorsResponse, Array<MonitorData>>(
+    "monitors",
+    (status, body) => {
+      const { data: monitors } = body;
+      if (status !== 200) {
+        console.error("Unexpected status code:", status);
+      }
+
+      if (!Array.isArray(monitors) || monitors.length <= 0) {
+        throw new Error("Failed to fetch all monitors", {
+          cause: { status, body },
+        });
+      }
+      return monitors;
+    },
   );
 }
