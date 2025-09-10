8d10e8683366ba7950ea8e75aff70d982425acbc
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-10 16:48:05 -0700

    Removes default handler

--- lib/account/details.ts
+++ lib/account/details.ts
@@ -2,11 +2,6 @@ import { apiCall } from "$http";
 import type { UserDetails } from "./types.ts";
 
 /**
- * The data structure returned from the "GET /account/" endpoint
- */
-export type DetailsResponse = { data: UserDetails };
-
-/**
  * Get details of a user from their given api token.
  *
  * @returns The raw GET "/account" endpoint response
@@ -14,22 +9,14 @@ export type DetailsResponse = { data: UserDetails };
 export async function getUserDetails(
   token: string,
 ): Promise<UserDetails> {
-  return await apiCall<{ data: UserDetails }, UserDetails>(
-    {
-      url: "account",
-      init: {
-        headers: {
-          "Accept": "application/json",
-          "Authorization": `Token ${token}`,
-          "Content-Type": "application/json",
-        },
+  return await apiCall<UserDetails>({
+    url: "account",
+    init: {
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `Token ${token}`,
+        "Content-Type": "application/json",
       },
     },
-    (status, body) => {
-      if (status !== 200) {
-        console.error("Unexpected status code:", status);
-      }
-      return body.data;
-    },
-  );
+  });
 }
--- lib/account/login.ts
+++ lib/account/login.ts
@@ -2,11 +2,6 @@ import { apiCall } from "$http";
 import type { UserDetails } from "./types.ts";
 
 /**
- * The data structure returned from the "/account/login/" endpoint
- */
-export type LoginResponse = { data: UserDetails };
-
-/**
  * Log in with the given user credentials.
  *
  * @returns The raw "/account/login" endpoint response
@@ -15,23 +10,15 @@ export async function login(
   identifier: string,
   password: string,
 ): Promise<UserDetails> {
-  return await apiCall<LoginResponse, UserDetails>(
-    {
-      url: "account/login",
-      init: {
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ identifier, password }),
+  return await apiCall<UserDetails>({
+    url: "account/login",
+    init: {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify({ identifier, password }),
     },
-    (status, body) => {
-      if (status !== 200) {
-        console.error("Unexpected status code:", status);
-      }
-      return body.data;
-    },
-  );
+  });
 }
