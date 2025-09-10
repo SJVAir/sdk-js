f0c9d76af76075db653ebfe2b9b8ff3c5123fbe8
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-09 17:20:08 -0700

    Updated to use new apiCall function

--- lib/account/details.ts
+++ lib/account/details.ts
@@ -1,5 +1,10 @@
-import { apiRequest, type APIRequestResponse, getApiUrl } from "$http";
+import { apiCall } from "$http";
 import type { UserDetails } from "./types.ts";
+
+/**
+ * The data structure returned from the "GET /account/" endpoint
+ */
+export type DetailsResponse = { data: UserDetails };
 
 /**
  * Get details of a user from their given api token.
@@ -8,15 +13,23 @@ import type { UserDetails } from "./types.ts";
  */
 export async function getUserDetails(
   token: string,
-): Promise<APIRequestResponse<{ data: UserDetails }>> {
-  return await apiRequest<{ data: UserDetails }>(
-    getApiUrl("account"),
+): Promise<UserDetails> {
+  return await apiCall<{ data: UserDetails }, UserDetails>(
     {
-      headers: {
-        "Accept": "application/json",
-        "Authorization": `Token ${token}`,
-        "Content-Type": "application/json",
+      url: "account",
+      init: {
+        headers: {
+          "Accept": "application/json",
+          "Authorization": `Token ${token}`,
+          "Content-Type": "application/json",
+        },
       },
+    },
+    (status, body) => {
+      if (status !== 200) {
+        console.error("Unexpected status code:", status);
+      }
+      return body.data;
     },
   );
 }
--- lib/account/login.ts
+++ lib/account/login.ts
@@ -1,5 +1,10 @@
-import { apiRequest, type APIRequestResponse, getApiUrl } from "$http";
+import { apiCall } from "$http";
 import type { UserDetails } from "./types.ts";
+
+/**
+ * The data structure returned from the "/account/login/" endpoint
+ */
+export type LoginResponse = { data: UserDetails };
 
 /**
  * Log in with the given user credentials.
@@ -9,16 +14,24 @@ import type { UserDetails } from "./types.ts";
 export async function login(
   identifier: string,
   password: string,
-): Promise<APIRequestResponse<{ data: UserDetails }>> {
-  return await apiRequest<{ data: UserDetails }>(
-    getApiUrl("account/login"),
+): Promise<UserDetails> {
+  return await apiCall<LoginResponse, UserDetails>(
     {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
+      url: "account/login",
+      init: {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ identifier, password }),
       },
-      body: JSON.stringify({ identifier, password }),
+    },
+    (status, body) => {
+      if (status !== 200) {
+        console.error("Unexpected status code:", status);
+      }
+      return body.data;
     },
   );
 }
