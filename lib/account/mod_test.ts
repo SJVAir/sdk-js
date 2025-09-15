3f7ccc0c582c8a467ed3dc9de387209176fd91ac
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-15 16:03:05 -0700

    Completes account testing

--- lib/account/mod_test.ts
+++ lib/account/mod_test.ts
@@ -1,7 +1,16 @@
-import { assertEquals, fail } from "@std/assert";
+import { assertEquals } from "@std/assert";
 import { setOrigin } from "$http";
-import { getSimpleValidationTest } from "$testing";
-import { passwordResetCredentialsSchema, userDetailsSchema } from "./schema.ts";
+import {
+  EXISTING_MONITOR_ID,
+  EXISTING_PHONE,
+  getSimpleValidationTest,
+} from "$testing";
+import {
+  monitorSubscriptionSchema,
+  monitorUnsubscribeResponseSchema,
+  passwordResetCredentialsSchema,
+  userDetailsSchema,
+} from "./schema.ts";
 import { createUser, type CreateUserForm } from "./create.ts";
 import { login } from "./login.ts";
 import { getUserDetails } from "./details.ts";
@@ -10,22 +19,34 @@ import { sendPhoneVerification, verifyPhone } from "./phone.ts";
 import { passwordResetRequest, resetPassword } from "./password-reset.ts";
 import { changePassword } from "./change-password.ts";
 import { deleteUser } from "./delete.ts";
-import type { PasswordResetCredentials, UserDetails } from "./types.ts";
+import { getSubscriptions, subscribe, unsubscribe } from "./subscriptions.ts";
+import type {
+  MonitorSubscription,
+  PasswordResetCredentials,
+  UserDetails,
+} from "./types.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
 }
 
+const validateMonitorSubscription = getSimpleValidationTest(
+  monitorSubscriptionSchema,
+);
+const validateMonitorUnsubscriberesponse = getSimpleValidationTest(
+  monitorUnsubscribeResponseSchema,
+);
 const validateUserDetails = getSimpleValidationTest(userDetailsSchema);
 const validatePasswordResetCreds = getSimpleValidationTest(
   passwordResetCredentialsSchema,
 );
 
+const delay = (delay: number) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
+
 const getRandomPhone = () =>
   Array.from({ length: 4 }, () => Math.floor(Math.random() * (8 - 1 + 1)) + 1)
     .join("");
-
-const getRandomPassword = () => `This is still bad ${getRandomPhone()}`;
 
 const promptCode = (phone: string) =>
   prompt(
@@ -35,143 +56,216 @@ const promptCode = (phone: string) =>
 const TEST_USER_FORM: CreateUserForm = {
   full_name: "TEST USER",
   password: "Th!$ !$ @ b4d p@$$w0rd",
-  phone: `559283${getRandomPhone()}`,
+  phone: EXISTING_PHONE,
   email: `${getRandomPhone()}@example.com`,
+};
+
+const getRandomPassword = () => {
+  TEST_USER_FORM.password = `This is still bad ${getRandomPhone()}`;
+  return TEST_USER_FORM.password;
 };
 
 Deno.test({
   name: "Module: Account Endpoints",
   permissions: { net: true },
   async fn(t) {
-    let createdUser: UserDetails;
-    let loginUser: UserDetails;
+    const crudSuccess = await t.step(
+      "Submodule: Account CRUD Operations",
+      async (t2) => {
+        let createdUser: UserDetails;
 
-    const canCreate = await t.step(
-      "POST account/register",
-      async () => {
-        createdUser = await createUser(TEST_USER_FORM);
-        validateUserDetails(createdUser);
-      },
-    );
-
-    await t.step({
-      name: "POST account/phone/",
-      ignore: !canCreate,
-      fn: async () => {
-        await sendPhoneVerification(createdUser.api_token);
-      },
-    });
-
-    await t.step({
-      name: "POST account/phone/verify/",
-      ignore: !canCreate,
-      fn: async () => {
-        const code = promptCode(createdUser.phone);
-        await verifyPhone(code, createdUser.api_token);
-      },
-    });
-
-    const canLogin = await t.step({
-      name: "POST account/login/",
-      ignore: !canCreate,
-      fn: async () => {
-        loginUser = await login(
-          TEST_USER_FORM.phone,
-          TEST_USER_FORM.password,
+        const canCreate = await t2.step(
+          "POST  account/register",
+          async () => {
+            createdUser = await createUser(TEST_USER_FORM);
+            validateUserDetails(createdUser);
+          },
         );
 
-        validateUserDetails(loginUser);
-      },
-    });
-
-    await t.step({
-      name: "GET  account/",
-      ignore: !canLogin,
-      async fn() {
-        const details = await getUserDetails(loginUser.api_token);
-
-        validateUserDetails(details);
-        assertEquals(
-          loginUser,
-          details,
-          "Login user object differs from details user object",
-        );
-      },
-    });
-
-    await t.step({
-      name: "PATCH  account/",
-      ignore: !canLogin,
-      async fn() {
-        const newEmail = "thisbethenewemail@example.com";
-        const updatedUser = await updateUser({
-          api_token: loginUser.api_token,
-          email: newEmail,
-        });
-
-        validateUserDetails(updatedUser);
-        assertEquals(newEmail, updatedUser.email);
-      },
-    });
-
-    await t.step({
-      name: "PUT  account/password",
-      ignore: !canLogin,
-      async fn() {
-        const newPass = getRandomPassword();
-        loginUser = await changePassword({
-          api_token: loginUser.api_token,
-          old_password: TEST_USER_FORM.password,
-          new_password1: newPass,
-          new_password2: newPass,
-        });
-
-        validateUserDetails(loginUser);
-      },
-    });
-
-    await t.step({
-      name: "Reset password",
-      ignore: !canLogin,
-      async fn(t2) {
-        let resetCreds: PasswordResetCredentials;
-
-        const resetRequestSuccess = await t2.step({
-          name: "POST  account/password-reset",
-          ignore: !canLogin,
+        await t2.step({
+          name: "POST  account/phone/verify/",
+          ignore: !canCreate,
           async fn() {
-            resetCreds = await passwordResetRequest(loginUser.phone);
-
-            validatePasswordResetCreds(resetCreds);
+            const code = promptCode(createdUser.phone);
+            await verifyPhone(code, createdUser.api_token);
           },
         });
 
         await t2.step({
-          name: "POST  account/password-reset/{UIDB64}/{TOKEN}",
-          ignore: !resetRequestSuccess,
+          name: "GET   account/",
+          ignore: !canCreate,
           async fn() {
-            const code: string = promptCode(loginUser.phone);
+            const details = await getUserDetails(createdUser.api_token);
 
-            const newPass = getRandomPassword();
-            await resetPassword({
-              new_password1: newPass,
-              new_password2: newPass,
-              code,
-              ...resetCreds,
+            validateUserDetails(details);
+          },
+        });
+
+        await t2.step({
+          name: "PATCH account/",
+          ignore: !canCreate,
+          async fn() {
+            const newEmail = "thisbethenewemail@example.com";
+            const updatedUser = await updateUser({
+              api_token: createdUser.api_token,
+              email: newEmail,
             });
+
+            validateUserDetails(updatedUser);
+            assertEquals(newEmail, updatedUser.email);
+          },
+        });
+        await t2.step({
+          name: "DEL   account/",
+          ignore: !canCreate,
+          async fn() {
+            await deleteUser(
+              TEST_USER_FORM.password,
+              createdUser.api_token,
+            );
           },
         });
       },
-    });
+    );
 
     await t.step({
-      name: "DEL  account/",
-      ignore: !canLogin,
-      async fn() {
-        await deleteUser(
-          TEST_USER_FORM.password,
-          loginUser.api_token,
-        );
+      name: "Submodule: Account Management",
+      ignore: !crudSuccess,
+      fn: async (t2) => {
+        const createdUser = await createUser(TEST_USER_FORM);
+        await delay(500);
+        let loginUser: UserDetails;
+
+        const canLogin = await t2.step({
+          name: "POST  account/login/",
+          ignore: !createdUser,
+          async fn() {
+            loginUser = await login(
+              TEST_USER_FORM.phone,
+              TEST_USER_FORM.password,
+            );
+
+            validateUserDetails(loginUser);
+          },
+        });
+
+        await t2.step({
+          name: "Submodule: Phone Verification",
+          ignore: !canLogin,
+          fn: async (t3) => {
+            const canSend = await t3.step({
+              name: "POST  account/phone/",
+              fn: async () => {
+                await sendPhoneVerification(loginUser.api_token);
+              },
+            });
+
+            await t3.step({
+              name: "POST  account/phone/verify/",
+              ignore: !canSend,
+              fn: async () => {
+                const code = promptCode(loginUser.phone);
+                await verifyPhone(code, loginUser.api_token);
+              },
+            });
+          },
+        });
+
+        await t2.step({
+          name: "Submodule: Monitor Subscriptions",
+          ignore: !canLogin,
+          async fn(t3) {
+            let subscriptions: Array<MonitorSubscription>;
+
+            const canSubscribe = await t3.step(
+              "Post  monitors/<MONITOR_ID>/alerts/subscribe/",
+              async () => {
+                const subscription = await subscribe({
+                  level: "unhealthy_sensitive",
+                  monitorId: EXISTING_MONITOR_ID,
+                  apiToken: loginUser.api_token,
+                });
+
+                validateMonitorSubscription(subscription);
+              },
+            );
+
+            await t3.step({
+              name: "GET   alerts/subscriptions",
+              ignore: !canSubscribe,
+              async fn() {
+                subscriptions = await getSubscriptions(loginUser.api_token);
+                validateMonitorSubscription(subscriptions);
+              },
+            });
+
+            await t3.step({
+              name: "Post  monitors/<MONITOR_ID>/alerts/unsubscribe/",
+              ignore: !Array.isArray(subscriptions!) ||
+                subscriptions.length <= 0,
+              async fn() {
+                const result = await unsubscribe({
+                  level: "unhealthy_sensitive",
+                  monitorId: EXISTING_MONITOR_ID,
+                  apiToken: loginUser.api_token,
+                });
+                validateMonitorUnsubscriberesponse(result);
+              },
+            });
+          },
+        });
+
+        await t2.step({
+          name: "PUT   account/password",
+          ignore: !canLogin,
+          async fn() {
+            const { password: old_password } = TEST_USER_FORM;
+            const newPass = getRandomPassword();
+            loginUser = await changePassword({
+              api_token: loginUser.api_token,
+              old_password,
+              new_password1: newPass,
+              new_password2: newPass,
+            });
+
+            validateUserDetails(loginUser);
+          },
+        });
+
+        await t2.step({
+          name: "Submodule: Password Reset",
+          ignore: !canLogin,
+          async fn(t3) {
+            let resetCreds: PasswordResetCredentials;
+
+            const resetRequestSuccess = await t3.step(
+              "POST  account/password-reset",
+              async () => {
+                resetCreds = await passwordResetRequest(loginUser.phone);
+
+                validatePasswordResetCreds(resetCreds);
+              },
+            );
+
+            await t3.step({
+              name: "POST  account/password-reset/{UIDB64}/{TOKEN}",
+              ignore: !resetRequestSuccess,
+              async fn() {
+                const code: string = promptCode(loginUser.phone);
+
+                const newpass = getRandomPassword();
+                await resetPassword({
+                  new_password1: newpass,
+                  new_password2: newpass,
+                  code,
+                  ...resetCreds,
+                });
+              },
+            });
+          },
+        });
+
+        await deleteUser(TEST_USER_FORM.password, loginUser!.api_token);
       },
     });
   },
