888be13a989822c8890870640fb207d9e0edbaff
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-14 17:21:59 -0700

    Updates account module exports

--- lib/account/mod.ts
+++ lib/account/mod.ts
@@ -1,3 +1,13 @@
-export { login } from "./login.ts";
+export { changePassword } from "./change-password.ts";
+export { createUser, type CreateUserForm } from "./create.ts";
+export { deleteUser } from "./delete.ts";
 export { getUserDetails } from "./details.ts";
+export { login } from "./login.ts";
+export {
+  type PasswordResetConfig,
+  passwordResetRequest,
+  resetPassword,
+} from "./password-reset.ts";
+export { sendPhoneVerification, verifyPhone } from "./phone.ts";
 export * from "./types.ts";
+export { updateUser, type UpdateUserForm } from "./update.ts";
