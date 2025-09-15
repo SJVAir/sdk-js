70cf4f33db345cbfe85625f2687f719424681567
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-15 16:02:42 -0700

    Adds monitor subscription types

--- lib/account/schema.ts
+++ lib/account/schema.ts
@@ -1,6 +1,51 @@
 import * as z from "zod";
 
-export interface UserDetailsSchema extends
+interface MonitorSubscriptionSchema extends
+  z.ZodObject<{
+    monitor: z.ZodString;
+    level: z.ZodEnum<{
+      unhealthy_sensitive: "unhealthy_sensitive";
+      unhealthy: "unhealthy";
+      very_unhealthy: "very_unhealthy";
+      hazardous: "hazardous";
+    }>;
+  }> {}
+
+export const monitorSubscriptionSchema: MonitorSubscriptionSchema = z.object({
+  monitor: z.string(),
+  level: z.enum([
+    "unhealthy_sensitive",
+    "unhealthy",
+    "very_unhealthy",
+    "hazardous",
+  ]),
+});
+
+interface MonitorUnsubscribeResponseSchema extends
+  z.ZodObject<{
+    success: z.ZodBoolean;
+  }> {}
+
+export const monitorUnsubscribeResponseSchema:
+  MonitorUnsubscribeResponseSchema = z.object({
+    success: z.boolean(),
+  });
+
+interface PasswordResetCredentialsSchema extends
+  z.ZodObject<{
+    /** The ID of the user */
+    uidb64: z.ZodString;
+    /** The full name of the user */
+    token: z.ZodString;
+  }> {}
+
+export const passwordResetCredentialsSchema: PasswordResetCredentialsSchema = z
+  .object({
+    uidb64: z.string(),
+    token: z.string(),
+  });
+
+interface UserDetailsSchema extends
   z.ZodObject<{
     /** The ID of the user */
     id: z.ZodString;
@@ -27,17 +72,3 @@ export const userDetailsSchema: UserDetailsSchema = z.object({
   language: z.enum(["en", "es", "hmn", "tl"]),
   api_token: z.string(),
 });
-
-export interface PasswordResetCredentialsSchema extends
-  z.ZodObject<{
-    /** The ID of the user */
-    uidb64: z.ZodString;
-    /** The full name of the user */
-    token: z.ZodString;
-  }> {}
-
-export const passwordResetCredentialsSchema: PasswordResetCredentialsSchema = z
-  .object({
-    uidb64: z.string(),
-    token: z.string(),
-  });
--- lib/account/types.ts
+++ lib/account/types.ts
@@ -1,11 +1,18 @@
 import type { infer as zinfer } from "zod";
 import type {
+  monitorSubscriptionSchema,
+  monitorUnsubscribeResponseSchema,
   passwordResetCredentialsSchema,
   userDetailsSchema,
 } from "./schema.ts";
 
-export type UserDetails = zinfer<typeof userDetailsSchema>;
+export type MonitorSubscription = zinfer<typeof monitorSubscriptionSchema>;
+export type MonitorUnsubscribeResponse = zinfer<
+  typeof monitorUnsubscribeResponseSchema
+>;
 
 export type PasswordResetCredentials = zinfer<
   typeof passwordResetCredentialsSchema
 >;
+
+export type UserDetails = zinfer<typeof userDetailsSchema>;
