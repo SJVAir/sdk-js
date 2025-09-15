f09c27109980a987ce7156c83bf06a3588d29a00
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-14 17:21:42 -0700

    Adds testing for password change and reset

--- lib/account/mod_test.ts
+++ lib/account/mod_test.ts
@@ -1,35 +1,36 @@
 import { assertEquals, fail } from "@std/assert";
-import { MailSlurp } from "mailslurp-client";
 import { setOrigin } from "$http";
 import { getSimpleValidationTest } from "$testing";
-import { userDetailsSchema } from "./schema.ts";
-import { createUser } from "./create.ts";
+import { passwordResetCredentialsSchema, userDetailsSchema } from "./schema.ts";
+import { createUser, type CreateUserForm } from "./create.ts";
 import { login } from "./login.ts";
 import { getUserDetails } from "./details.ts";
-import type { UserDetails } from "./types.ts";
-import type { CreateUserForm } from "./create.ts";
-import { deleteUser } from "./delete.ts";
 import { updateUser } from "./update.ts";
 import { sendPhoneVerification, verifyPhone } from "./phone.ts";
+import { passwordResetRequest, resetPassword } from "./password-reset.ts";
+import { changePassword } from "./change-password.ts";
+import { deleteUser } from "./delete.ts";
+import type { PasswordResetCredentials, UserDetails } from "./types.ts";
 
 if (!Deno.env.has("TEST_REMOTE")) {
   setOrigin("http://127.0.0.1:8000");
 }
 
 const validateUserDetails = getSimpleValidationTest(userDetailsSchema);
+const validatePasswordResetCreds = getSimpleValidationTest(
+  passwordResetCredentialsSchema,
+);
 
-async function getEmail() {
-  const mailslurp = new MailSlurp({
-    apiKey: "7f420de8a51a5ed71c04b6445b2da4b39b4879d0a6ee423fd65a2eab48a37d46",
-  });
-  const inbox = await mailslurp.inboxController.createInboxWithDefaults();
-  const emailPagination = await mailslurp.emailController.getEmailsPaginated({
-    inboxId: [inbox.id],
-  });
-}
 const getRandomPhone = () =>
   Array.from({ length: 4 }, () => Math.floor(Math.random() * (8 - 1 + 1)) + 1)
     .join("");
+
+const getRandomPassword = () => `This is still bad ${getRandomPhone()}`;
+
+const promptCode = (phone: string) =>
+  prompt(
+    `Please enter the code that was sent to the phone number "${phone}"`,
+  )!;
 
 const TEST_USER_FORM: CreateUserForm = {
   full_name: "TEST USER",
@@ -65,10 +66,8 @@ Deno.test({
       name: "POST account/phone/verify/",
       ignore: !canCreate,
       fn: async () => {
-        const code = "";
+        const code = promptCode(createdUser.phone);
         await verifyPhone(code, createdUser.api_token);
-
-        fail("Phone verification testing needs to be figured out still");
       },
     });
 
@@ -112,6 +111,56 @@ Deno.test({
 
         validateUserDetails(updatedUser);
         assertEquals(newEmail, updatedUser.email);
+      },
+    });
+
+    await t.step({
+      name: "PUT  account/password",
+      ignore: !canLogin,
+      async fn() {
+        const newPass = getRandomPassword();
+        loginUser = await changePassword({
+          api_token: loginUser.api_token,
+          old_password: TEST_USER_FORM.password,
+          new_password1: newPass,
+          new_password2: newPass,
+        });
+
+        validateUserDetails(loginUser);
+      },
+    });
+
+    await t.step({
+      name: "Reset password",
+      ignore: !canLogin,
+      async fn(t2) {
+        let resetCreds: PasswordResetCredentials;
+
+        const resetRequestSuccess = await t2.step({
+          name: "POST  account/password-reset",
+          ignore: !canLogin,
+          async fn() {
+            resetCreds = await passwordResetRequest(loginUser.phone);
+
+            validatePasswordResetCreds(resetCreds);
+          },
+        });
+
+        await t2.step({
+          name: "POST  account/password-reset/{UIDB64}/{TOKEN}",
+          ignore: !resetRequestSuccess,
+          async fn() {
+            const code: string = promptCode(loginUser.phone);
+
+            const newPass = getRandomPassword();
+            await resetPassword({
+              new_password1: newPass,
+              new_password2: newPass,
+              code,
+              ...resetCreds,
+            });
+          },
+        });
       },
     });
 
