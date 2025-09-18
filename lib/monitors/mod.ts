dffd3f74984ad90b50854a86a1262d15555c981a
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-18 10:07:38 -0700

    adds get_monitors_meta to exports

--- lib/monitors/mod.ts
+++ lib/monitors/mod.ts
@@ -4,4 +4,5 @@ export * from "./get_monitor_entries.ts";
 export * from "./get_monitor_entries_csv.ts";
 export * from "./get_monitors.ts";
 export * from "./get_monitors_latest.ts";
+export * from "./get_monitors_meta.ts";
 export * from "./types.ts";
