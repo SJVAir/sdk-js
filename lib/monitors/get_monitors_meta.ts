67c02bd7859dba8a94cff9093d8425a1ca84f858
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-22 13:55:05 -0700

    Adds types and annotations to avoid slow types

--- lib/monitors/get_monitors_meta.ts
+++ lib/monitors/get_monitors_meta.ts
@@ -9,47 +9,87 @@ import type {
 } from "./types.ts";
 
 class EntriesMeta implements SJVAirEntryMeta {
+  /**
+   * An iterable representation of the entry level metadata.
+   */
   asIter: {
+    /**
+     * An array of entry level metadata.
+     */
     levels: Array<SJVAirEntryLevel>;
   };
 
+  /**
+   * Creates an instance of EntriesMeta.
+   */
   constructor(private meta: SJVAirEntryMeta) {
     this.asIter = {
       levels: Object.values(this.meta.levels ?? {}),
     };
   }
 
-  get epa_aqs_code() {
+  /**
+   * The EPA AQS code associated with this entry type.
+   */
+  get epa_aqs_code(): number | null {
     return this.meta.epa_aqs_code;
   }
 
-  get fields() {
+  /**
+   * The data fields available with this entry type.
+   */
+  get fields(): Array<string> {
     return this.meta.fields;
   }
 
-  get label() {
+  /**
+   * The human-readable label for this entry type.
+   */
+  get label(): string {
     return this.meta.label;
   }
 
-  get levels() {
+  /**
+   * The intensity levels associated with this entry type.
+   */
+  get levels(): Record<string, SJVAirEntryLevel> | null {
     return this.meta.levels;
   }
 
-  get type() {
+  /**
+   * The type of monitor entry (e.g., "pm25", "o3", etc.).
+   */
+  get type(): string {
     return this.meta.type;
   }
 
-  get units() {
+  /**
+   * The symbol(s) used to represent the unit of measurement for this entry type.
+   */
+  get units(): string | null {
     return this.meta.units;
   }
 }
 
 export class MonitorsMeta implements SJVAirMonitorsMeta {
+  /**
+   * An iterable representation of the monitor metadata.
+   */
   asIter: {
+    /**
+     * An array of monitor entry metadata.
+     */
     entries: Array<SJVAirEntryMeta>;
+
+    /**
+     * An array of monitor metadata.
+     */
     monitors: Array<SJVAirMonitorDeviceMeta>;
   };
 
+  /**
+   * Creates an instance of MonitorsMeta.
+   */
   constructor(private meta: SJVAirMonitorsMeta) {
     this.asIter = {
       entries: Object.values(this.meta.entries),
@@ -57,25 +97,53 @@ export class MonitorsMeta implements SJVAirMonitorsMeta {
     };
   }
 
-  get default_pollutant() {
+  /**
+   * Returns the default pollutant for monitors.
+   *
+   * @returns The default pollutant for monitors.
+   */
+  get default_pollutant(): SJVAirMonitorsMeta["default_pollutant"] {
     return this.meta.default_pollutant;
   }
 
-  get entries() {
+  /**
+   * Returns a record of all monitor entry metadata.
+   *
+   * @returns A record of all monitor entry metadata.
+   */
+  get entries(): Record<string, SJVAirEntryMeta> {
     return this.meta.entries;
   }
 
-  get monitors() {
+  /**
+   * Returns an record of all monitor metadata.
+   *
+   * @returns A record of all monitor metadata.
+   */
+  get monitors(): Record<string, SJVAirMonitorDeviceMeta> {
     return this.meta.monitors;
   }
 
-  entryType(entryType: MonitorEntryType) {
+  /**
+   * Returns the metadata for a specific monitor entry type.
+   *
+   * @param entryType - The type of monitor entry to get the metadata for.
+   * @returns The metadata for the specified monitor entry type.
+   */
+  entryType(entryType: MonitorEntryType): EntriesMeta {
     const entryMeta = this.asIter.entries.find((entryMeta) =>
       entryMeta.type === entryType
     )!;
     return new EntriesMeta(entryMeta);
   }
-  monitorType(monitorType: MonitorType) {
+
+  /**
+   * Returns the metadata for a specific monitor type.
+   *
+   * @param monitorType - The type of monitor to get the metadata for.
+   * @returns The metadata for the specified monitor type.
+   */
+  monitorType(monitorType: MonitorType): SJVAirMonitorDeviceMeta {
     return this.asIter.monitors.find((monitorMeta) =>
       monitorMeta.type === monitorType
     )!;
